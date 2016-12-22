16e2c3d33cc5a9f387646d5ea14390264576f5a6
Samuel Heer <samuel.heer@diging.de>
2016-12-22 22:25:36 +0100

    added version bumping to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var composer_json         = require('./composer.json');
 var gulp          = require('gulp');
 var plumber       = require('gulp-plumber');
 var sass          = require('gulp-sass');
@@ -12,24 +13,31 @@ var cleanCSS      = require('gulp-clean-css');
 var sourcemaps    = require('gulp-sourcemaps');
 var browserSync   = require("browser-sync").create();
 var concat        = require('gulp-concat');
+var bump          = require('gulp-bump');
+var replace       = require('gulp-string-replace');
+var exec            = require('child_process').exec;
 
 
 // browser sync proxy url: e.g. a vhost-based url,
 // see also: https://www.browsersync.io/docs/options#option-proxy
 var bsProxy       = "http://localhost:8888/projects";
 
+var version = composer_json.version;
+
 var paths = {
     src: {
         styles:     'src/scss/default.scss',
         scripts:    ['node_modules/object-fit-images/dist/ofi.browser.js','src/js/**/*.js'],
         images:     'src/img/**/*',
-        fonts:      'src/fonts/**/*'
+        fonts:      'src/fonts/**/*',
+        templates:  'src/templates/**/*'
     },
     dist: {
-        styles:     'dist/statics/css',
-        scripts:    'dist/statics/js',
-        images:     'dist/statics/img',
-        fonts:      'dist/statics/fonts'
+        styles:     'dist/static/'+version+'/css',
+        scripts:    'dist/static/'+version+'/js',
+        images:     'dist/static/'+version+'/img',
+        fonts:      'dist/static/'+version+'/fonts',
+        templates:  'dist/TL_ROOT/templates/etgwebkit'
     },
     watch: {
         styles:     'src/scss/**/*.scss',
@@ -86,6 +94,12 @@ gulp.task('images', function () {
         .pipe(gulp.dest(paths.dist.images));
 });
 
+gulp.task('templates',function(){
+   return gulp.src(paths.src.templates)
+       .pipe(replace('{{version}}',composer_json.version))
+       .pipe(gulp.dest(paths.dist.templates))
+});
+
 // copy static files from src to dist
 gulp.task('copy', function() {
    gulp.src(paths.src.fonts)
@@ -105,5 +119,24 @@ gulp.task('serve', ['styles'], function() {
     gulp.watch(paths.watch.scripts, ['scripts']).on('change', browserSync.reload);
 });
 
+//bumper
+gulp.task('bump', function(){
+    gulp.src(['./package.json','./composer.json'])
+        .pipe(bump())
+        //.pipe(bump("major"))
+        //.pipe(bump("minor"))
+        //.pipe(bump("patch")) //default
+        .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('adjustLatestSymlink', function(){
+
+    exec('rm dist/static/latest');
+    console.log(version);
+    exec('ln -s dist dist/static/latest');
+});
+
 gulp.task('default', ['serve']);
-gulp.task('deploy', ['minify_css', 'scripts', 'images']);
+gulp.task('deploy', ['minify_css', 'scripts', 'images','templates']);
+gulp.task('version', ['bump','deploy','adjustLatestSymlink']);
